431fa2dd6375fdca000e29bfc4bea156161e2e8a
Savaş Dede <svsdd22@gmail.com>
2023-08-13 16:29:34 +0300

    leaflet buttons

--- angular-leaflet/src/app/pages/leaflet/home/home.component.ts
+++ angular-leaflet/src/app/pages/leaflet/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router,
+    private route: ActivatedRoute,) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  landmark() {
+    this.router.navigate(['/leaflet/landmark'], {
+      queryParams: {
+        data:null,
+      }
+    });
+  }
+
+  routing() {
+    this.router.navigate(['/leaflet/landmark'], {
+      queryParams: {
+        data:null,
+      }
+    });
+  }
+
+  antPath() {
+    this.router.navigate(['/leaflet/landmark'], {
+      queryParams: {
+        data:null,
+      }
+    });
+  }
+
+  geofence() {
+    this.router.navigate(['/leaflet/landmark'], {
+      queryParams: {
+        data:null,
+      }
+    });
+  }
+
 
 }
